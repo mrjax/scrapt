509c8faacb027f7fbdbd18770504d8aba829f216
mrjax <quesomonos@gmail.com>
2015-12-02 12:28:28 -0600

    Tweaked shortcut
    newlines

--- Scrapt.py
+++ Scrapt.py
@@ -5,5 +5,5 @@ class ScraptCommand(sublime_plugin.TextCommand):
 	def run(self, edit, **args):
 	
 		if args['op'] == 'date':
-			self.view.insert(edit, self.view.sel()[0].begin(), strftime("%m/%d/%y", gmtime()) + "\n" + strftime("%A", gmtime()) + "\n-------")
+			self.view.insert(edit, self.view.sel()[0].begin(), strftime("%m/%d/%y", gmtime()) + "\n" + strftime("%A", gmtime()) + "\n-------\n\n")
 
